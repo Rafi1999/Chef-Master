fdc86eef616df113b48b94dc240a84afb56b1d8b
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-05-04 15:02:45 +0600

    active route

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,12 +7,30 @@ const Header = () => {
     const location = useLocation()
     const [show,setShow] = useState(false);
     return (
-        <nav className="navbar bg-gray-300 font-medium flex justify-between">
+        <nav className="navbar bg-gray-300 font-medium flex justify-between py-4">
         <h3 className='text-3xl font-sans font-medium hover:text-orange-400'><MdFoodBank className='text-orange-500 text-5xl'></MdFoodBank>Chef-Master-Bangladesh</h3>
         <div className='flex gap-7 mr-5'>
-        <NavLink to='/'  className='hover:text-red-500'>Home</NavLink>
-        <NavLink to='/blog'  className='hover:text-red-500'>Blog</NavLink>
-        {user ? <div className='flex gap-2'><img className='rounded-lg w-10' src={user.photoURL} onMouseEnter={()=>setShow(true)} onMouseLeave={()=>setShow(false)}></img>{show && <p className='relative top-2'>{user.displayName}</p>}<button onClick={logOut}>LogOut</button> </div> : <button><NavLink to='/login' className='hover:text-red-500'>Login</NavLink></button>}
+        <NavLink to='/' className={({ isActive, isPending }) =>
+                      isActive
+                        ? "text-blue-600"
+                        : isPending
+                        ? "pending"
+                        : "hover:text-red-500"
+                    } >Home</NavLink>
+        <NavLink to='/blog'  className={({ isActive, isPending }) =>
+                      isActive
+                        ? "text-blue-600"
+                        : isPending
+                        ? "pending"
+                        : "hover:text-red-500"
+                    }>Blog</NavLink>
+        {user ? <div className='flex gap-2'><img className='relative rounded-full w-12' src={user.photoURL} onMouseEnter={()=>setShow(true)} onMouseLeave={()=>setShow(false)}></img>{show && <p className='relative top-8'>{user.displayName}</p>}<button onClick={logOut}>LogOut</button> </div> : <button><NavLink to='/login' className={({ isActive, isPending }) =>
+                      isActive
+                        ? "text-blue-600"
+                        : isPending
+                        ? "pending"
+                        : "hover:text-red-500"
+                    }>Login</NavLink></button>}
         
         </div>
         </nav>
